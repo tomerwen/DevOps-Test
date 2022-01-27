4eab8377b9a79bc0ca43747e0287ffb6f5409b24
tomerwen <tomerwen@gmail.com>
2022-01-27 06:27:48 +0200

    pull

--- TEST.py
+++ TEST.py
@@ -1,2 +1,39 @@
-print ("Hello World!")
-print ("Another Hello World!")
+import random
+class Node:
+   def __init__(self, dataval=None):
+      self.dataval = dataval
+      self.nextval = None
+
+class SLinkedList:
+   def __init__(self):
+      self.headval = None
+   def listprint(self):
+      printval = self.headval
+      while printval is not None:
+         print (printval.dataval)
+         printval = printval.nextval
+   def AtBegining(self,newdata):
+      NewNode = Node(newdata)
+      NewNode.nextval = self.headval
+      self.headval = NewNode
+    def AddNode(self , dataval)
+        self.dataval = dataval
+        self.nextval = None
+
+def Dice():
+    result = random.randint(1,6)
+    return result
+
+list = SLinkedList()
+list.headval = Node(Dice())
+node = Node(Dice())
+list.headval.nextval = node
+newNode = Node(Dice())
+node.nextval = newNode
+node = Node(Dice())
+newNode.nextval = node
+newNode = Node(Dice())
+node.nextval = newNode
+
+
+list.listprint()
