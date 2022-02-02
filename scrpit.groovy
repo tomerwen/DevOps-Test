480e135ecc94129ad315cd23bef11c49cd24ab85
tomerwen <30783660+tomerwen@users.noreply.github.com>
2022-02-02 08:13:53 +0200

    Update scrpit.groovy

--- scrpit.groovy
+++ scrpit.groovy
@@ -5,10 +5,10 @@ def buildJar() {
 
 def buildImage() {
     echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t nanajanashia/demo-app:jma-2.0 .'
-        sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push nanajanashia/demo-app:jma-2.0'
+        withCredentials([usernamePassword(credentialsId: 'jaanuk-docker', passwordVariable: 'PASS', usernameVariable: 'USER')])
+        sh "docker build -t jaanuk/devops:Test-1.0 ."
+        sh "echo PASS | docker login -u $USER --password-stdin "
+        sh "docker push jaanuk/devops:Test-1.0"
     }
 } 
 
